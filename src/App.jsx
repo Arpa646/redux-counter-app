9b7531cbc4185a81c979f71089b8b5851bcef929
arpa646 <sadikahosen50@gmail.com>
2023-07-28 11:26:31 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,51 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import store from './componant/store/Store'
+import { decrement, increment, reset } from './componant/action/Action'
 
 function App() {
-const conter=useSelector((store)=>store.name);
-const dispatch=useDispatch()
-console.log(conter)
+  const conter = useSelector((store) => store.count);
+  const dispatch = useDispatch()
+  const increase = () => {
+    console.log('hi')
+    dispatch(increment())
+  }
+  const decrease = () => {
+    console.log('hi')
+    dispatch(decrement())
+  }
+  const restart = () => {
+    console.log('hi')
+    dispatch(reset())
+  }
+
 
   return (
-   <>
- <h1>   counter:0</h1>
-    <button onClick={}>increase</button>
-   </>
+    //    <div className='card'>
+    //  <h1>   counter:{conter}</h1>
+    //     <button onClick={increase}>Increase</button>
+    //     <button onClick={decrease}>Decrease</button>
+    //     <button onClick={restart}>Reset</button>
+    //    </div>
+
+
+
+
+    <div class="container">
+      <div class="wrapper">
+        <div class="banner-image">
+          <h2>   Counter :{conter}</h2>
+        </div>
+
+      </div>
+      <div class="button-wrapper">
+
+        <button class="btn outline" onClick={increase}>increase</button>
+        <button class="btn fill" onClick={decrease}>decrease</button>
+        <button class="btn fill" onClick={restart}>restart</button>
+      </div>
+    </div>
+
   )
 }
 
